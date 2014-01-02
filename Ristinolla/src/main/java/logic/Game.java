1441c46274343a434485d3a1027e9f2e62ac1453
sorja <miro.sorja@gmail.com>
2014-01-02 16:08:58 +0200

    Updated java doc a little, started working on sequence diagram (no camera), problems with pit and cobertura, to be continued...

--- Ristinolla/src/main/java/logic/Game.java
+++ Ristinolla/src/main/java/logic/Game.java
@@ -1,25 +1,38 @@
 package logic;
 
+/**
+ *
+ * @author miro
+ */
 public class Game {
 
-    private GameBoard _gB;
+    private GameBoard _gameBoard;
     private boolean _playerTurn;
 
+    /**
+     * Setting gameboard and playerturn.
+     * @param gB
+     */
     public Game(GameBoard gB) {
-        _gB = gB;
+        _gameBoard = gB;
         _playerTurn = false;
     }
 
+    /**
+     * Continues the game 1 turn forwards
+     * @param x 
+     * @param y 
+     */
     public void turn(int x, int y) {
 
-        if (_gB.getSquareAtXY(x, y).getSquareState() != SquareState.EMPTY) {
+        if (_gameBoard.getSquareAtXY(x, y).getSquareState() != SquareState.EMPTY) {
             return;
         }
 
         if (_playerTurn) {
-            _gB.getSquareAtXY(x, y).setSquareState(SquareState.CROSS);
+            _gameBoard.getSquareAtXY(x, y).setSquareState(SquareState.CROSS);
         } else {
-            _gB.getSquareAtXY(x, y).setSquareState(SquareState.CIRCLE);
+            _gameBoard.getSquareAtXY(x, y).setSquareState(SquareState.CIRCLE);
         }
 
         checkWinningCondition(x, y);
@@ -32,8 +45,13 @@ public class Game {
 //
 //        return false;
 //    }
-    public GameBoard getGameBoard() {
-        return _gB;
+
+    /**
+     * Returns gameboard
+     * @return
+     */
+        public GameBoard getGameBoard() {
+        return _gameBoard;
     }
 
     private void checkWinningCondition(int x, int y) {
@@ -47,18 +65,14 @@ public class Game {
 
         sequence = isWinningFromRight(s, x, y) ? 3 : 0;
         sequence = isWinningFromLeft(s, x, y) ? 3 : 0;
-        sequence = isWinningFromMiddle(s, x, y) ? 3 : 0;
-
-        if (x == 0) {
-
-        }
+        sequence = isWinningFromMiddleX(s, x, y) ? 3 : 0;
 
         return sequence == 3;
     }
 
     private boolean isWinningFromRight(SquareState s, int x, int y) {
-        if (x == _gB.getWidth() - 1) {
-            if (_gB.getSquareAtXY(x - 1, y).getSquareState() == s && _gB.getSquareAtXY(x - 2, y).getSquareState() == s) {
+        if (x == _gameBoard.getWidth() - 1) {
+            if (_gameBoard.getSquareAtXY(x - 1, y).getSquareState() == s && _gameBoard.getSquareAtXY(x - 2, y).getSquareState() == s) {
                 return true;
             }
         }
@@ -67,28 +81,93 @@ public class Game {
 
     private boolean isWinningFromLeft(SquareState s, int x, int y) {
         if (x == 0) {
-            if (_gB.getSquareAtXY(x + 1, y).getSquareState() == s && _gB.getSquareAtXY(x + 2, y).getSquareState() == s) {
+            if (_gameBoard.getSquareAtXY(x + 1, y).getSquareState() == s && _gameBoard.getSquareAtXY(x + 2, y).getSquareState() == s) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean isWinningFromMiddle(SquareState s, int x, int y) {
-        if (x != 0 && x != _gB.getWidth() - 1) {
-            if (_gB.getSquareAtXY(x + 1, y).getSquareState() == s && _gB.getSquareAtXY(x - 1, y).getSquareState() == s) {
+    private boolean isWinningFromMiddleX(SquareState s, int x, int y) {
+        if (x != 0 && x != _gameBoard.getWidth() - 1) {
+            if (_gameBoard.getSquareAtXY(x + 1, y).getSquareState() == s && _gameBoard.getSquareAtXY(x - 1, y).getSquareState() == s) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean isVerticalWinning(SquareState s) {
+    private boolean isVerticalWinning(SquareState s, int x, int y) {
+        int sequence = 0;
+
+        sequence = isWinningFromBottom(s, x, y) ? 3 : 0;
+        sequence = isWinningFromTop(s, x, y) ? 3 : 0;
+        sequence = isWinningFromMiddleY(s, x, y) ? 3 : 0;
+
+        return sequence == 3;
+    }
+
+    private boolean isWinningFromBottom(SquareState s, int x, int y) {
+        if (y == _gameBoard.getHeight() - 1) {
+            if (_gameBoard.getSquareAtXY(x, y - 1).getSquareState() == s && _gameBoard.getSquareAtXY(x, y - 2).getSquareState() == s) {
+                return true;
+            }
+        }
         return false;
     }
 
-    private boolean isDiagonalWinning(SquareState s) {
+    private boolean isWinningFromTop(SquareState s, int x, int y) {
+        if (y == 0) {
+            if (_gameBoard.getSquareAtXY(x, y + 1).getSquareState() == s && _gameBoard.getSquareAtXY(x, y + 2).getSquareState() == s) {
+                return true;
+            }
+        }
         return false;
     }
 
+    private boolean isWinningFromMiddleY(SquareState s, int x, int y) {
+        if (y != 0 && y != _gameBoard.getHeight() - 1) {
+            if (_gameBoard.getSquareAtXY(x, y + 1).getSquareState() == s && _gameBoard.getSquareAtXY(x, y - 1).getSquareState() == s) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isDiagonalWinning(SquareState s, int x, int y) {
+        int sequence = 0;
+
+        sequence = isWinningFromBottomRight(s, x, y) ? 3 : 0;
+        sequence = isWinningFromLeft(s, x, y) ? 3 : 0;
+        sequence = isWinningFromMiddleX(s, x, y) ? 3 : 0;
+
+        return sequence == 3;
+    }
+
+    private boolean isWinningFromBottomRight(SquareState s, int x, int y) {
+        if (x == _gameBoard.getWidth() - 1 && y == _gameBoard.getHeight() - 1) {
+            if (_gameBoard.getSquareAtXY(x, y - 1).getSquareState() == s && _gameBoard.getSquareAtXY(x, y - 2).getSquareState() == s) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isWinningFromTopLeft(SquareState s, int x, int y) {
+        if (x == 0 && y == 0) {
+            if (_gameBoard.getSquareAtXY(x + 1, y + 1).getSquareState() == s && _gameBoard.getSquareAtXY(x + 2, y + 2).getSquareState() == s) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isWinningFromAnyMiddle(SquareState s, int x, int y) {
+        if (y != 0 && y != _gameBoard.getHeight() - 1 && x != 0 && x != _gameBoard.getWidth() - 1) {
+            if (_gameBoard.getSquareAtXY(x + 1, y + 1).getSquareState() == s && _gameBoard.getSquareAtXY(x - 1, y - 1).getSquareState() == s) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- Ristinolla/src/main/java/logic/Square.java
+++ Ristinolla/src/main/java/logic/Square.java
@@ -1,18 +1,33 @@
 
 package logic;
 
+/**
+ *
+ * @author miro
+ */
 public class Square {
 
     private SquareState _squareState;
 
+    /**
+     * Initializes the square state to empty
+     */
     public Square() {
         _squareState = SquareState.EMPTY;
     }
 
+    /**
+     *
+     * @return
+     */
     public SquareState getSquareState() {
         return _squareState;
     }
 
+    /**
+     *
+     * @param squareState
+     */
     public void setSquareState(SquareState squareState) {
         _squareState = squareState;
     }
