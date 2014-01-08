98cc13c2d405a5680a5cacbed7ccfe95d0c73dd7
sorja <miro.sorja@gmail.com>
2014-01-08 22:34:58 +0200

    Started UI

--- Ristinolla/src/main/java/ristinolla/Main.java
+++ Ristinolla/src/main/java/ristinolla/Main.java
@@ -1,6 +1,8 @@
 package ristinolla;
 
+import ristinolla.logic.Game;
 import ristinolla.logic.GameBoard;
+import ristinolla.ui.Ui;
 
 /**
  * Hello world!
@@ -10,6 +12,8 @@ public class Main
 {
     public static void main( String[] args )
     {
-        
+        Game g = new Game(new GameBoard(3, 3));
+        Ui u = new Ui(g);
+        u.run();
     }
 }
